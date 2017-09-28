2f42e031fa1e83fa88832086569286d6e4782dc8
IntiDev <yeseniase@outlook.it>
2017-09-28 17:28:43 -0500

    Se modifica solución del ejercicio aplicando la propiedad classList en la sentencia condicional.

--- menu_hamburguesa/app.js
+++ menu_hamburguesa/app.js
@@ -2,17 +2,13 @@ var btn = document.getElementById("menu");
 
 function mostrarMenu() {
   var menu = document.getElementById("opcs-menu");
-  var propCSS = window.getComputedStyle(menu).getPropertyValue("display");
-//  console.log(propCSS);
+  //console.log(menu.classList.contains());
 
-  if(propCSS == "none"){//menu.style.display  == "none" || menu.style.display  == ""
-    //console.log("mostrar");
-    //window.getComputedStyle(menu,"display").setPropertyValue("block");
-    menu.style.display = "block";
+  if(menu.classList.contains("noActive")){
+    menu.classList.remove("noActive");
   }
   else{
-    //console.log("ocultar");
-    menu.style.display = "none";
+    menu.classList.add("noActive");
   }
 }
 
